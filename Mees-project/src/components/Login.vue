a7a35c522ab3299a931c008df71c522b72822603
SandXs <archies559@gmail.com>
2023-09-18 12:12:13 +0200

    Login supabase setup v0.1

--- Mees-project/src/components/Login.vue
+++ Mees-project/src/components/Login.vue
@@ -4,4 +4,16 @@
 </template>
 
 <script setup>
+    async function signInWithEmail() {
+        const { data, error } = await supabase.auth.signInWithOtp({
+            email: 'archies559@gmail.com',
+            options: {
+            //emailRedirectTo: 'localhost:',
+            },
+        })
+    }
+    async function signOut() {
+        const { error } = await supabase.auth.signOut()
+    }
+
 </script>
