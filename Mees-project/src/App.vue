a620f6ee0ef679188b44c521550eadf33601101c
Jack Kartoush <jackkartosh@gmail.com>
2023-09-19 13:04:08 +0200

    addjustting Auth + Account components to log in and update data

--- Mees-project/src/App.vue
+++ Mees-project/src/App.vue
@@ -1,7 +1,28 @@
+
+
 <template>
-  <Login />
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
+  </div>
 </template>
 
 <script setup>
-  import Login from "./components/Login.vue"
+import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
+
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+   
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
 </script>
