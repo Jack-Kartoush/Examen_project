88807bdd15e6ec6f337570629fb5c415652766b6
SandXs <archies559@gmail.com>
2023-09-19 11:39:55 +0200

    Aanpassing login test fase, 2 nieuwe components

--- Mees-project/src/components/Login.vue
+++ Mees-project/src/components/Login.vue
@@ -1,26 +1,42 @@
 <template>
-  <label for="email">Email</label>
+    <form action="" @submit.prevent="signInWithEmail()">
+        <label for="email">Email</label>
   <br />
   <input type="text" v-model="email" id="email" />
   <button @click="signInWithEmail">login</button>
+    </form>
   
+  
+    <ul>
+      <li v-for="admin in Admin_Users" :key="admin.admin_id">{{ admin.admin_namefirst }}</li>
+    </ul>
 </template>
 
 <script setup>
 import { supabase } from '../supabase';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+
+const Admin_Users = ref([])
 const email = ref("")
 
-
+onMounted(() => {
+    Validator()
+})
+async function Validator() {
+    const { validator, err } = await supabase.from('Admin_Users').select("*")
+    Admin_Users.value = validator
+    // console.log(Admin_Users.value)
+}
 
 async function signInWithEmail() {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      emailRedirectTo: 'localhost:5173',
-    },
-  });
-  email.value = null
+    //const { validator, err } = await supabase.from('Admin_Users').select()
+    const { data, error } = await supabase.auth.signInWithOtp({
+        email: email.value,
+        options: {
+            emailRedirectTo: 'localhost:5173',
+        },
+    });
+    email.value = null
 }
 async function signOut() {
   const { error } = await supabase.auth.signOut();
