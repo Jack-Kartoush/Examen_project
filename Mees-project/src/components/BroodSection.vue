9be70c95e5a6479ddd2748e78571f93a60b79add
KevinScheper <kevin.scheper@hotmail.nl>
2023-09-25 09:28:10 +0200

    Slideshow toegevoegd

--- Mees-project/src/components/BroodSection.vue
+++ Mees-project/src/components/BroodSection.vue
@@ -1,4 +1,47 @@
 <template>
+<div class="container">
+            <div class="slider">
+                <input type="radio" class="radio" name="images" id="radio-1" checked>
+                <input type="radio" class="radio" name="images" id="radio-2" checked>
+                <input type="radio" class="radio" name="images" id="radio-3" checked>
+                <input type="radio" class="radio" name="images" id="radio-4" checked>
+                <input type="radio" class="radio" name="images" id="radio-5" checked>
+
+                <div class="slide" id="slide-1">
+                    <img src="../assets/img/broodje2.webp" alt="">
+                    <div class="caption"><span></span></div>
+                </div>
+                <div class="slide" id="slide-2">
+                    <img src="../assets/img/broodje.jpg" alt="">
+                    <div class="caption"><span></span></div>
+                </div>
+                <div class="slide" id="slide-3">
+                    <img src="image/image3.jpg" alt="">
+                    <div class="caption"><span></span></div>
+                </div>
+                <div class="slide" id="slide-4">
+                    <img src="image/image4.jpg" alt="">
+                    <div class="caption"><span></span></div>
+                </div>
+                <div class="slide" id="slide-5">
+                    <img src="image/image5.jpg" alt="">
+                    <div class="caption"><span></span></div>
+                </div>
+                
+                <div class="dots">
+                    <label for="radio-1" id="label-1"></label>
+                    <label for="radio-2" id="label-2"></label>
+                    <label for="radio-3" id="label-3"></label>
+                    <label for="radio-4" id="label-4"></label>
+                    <label for="radio-5" id="label-5"></label>
+
+                </div>
+
+            </div>
+            
+        </div>
+
+
 <section class="Vega-Broodjes" id="VegaBroodjes" >
     section 1
 </section>
@@ -16,3 +59,4 @@
 </section>
 
 </template>
+
