5c6e3fcb45b9a63e1ffc140343dda0e242e5ce96
Jack Kartoush <jackkartosh@gmail.com>
2023-09-25 14:31:56 +0200

    addjusting app

--- Mees-project/src/App.vue
+++ Mees-project/src/App.vue
@@ -2,24 +2,27 @@
 
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
+  
     <Account v-if="session" :session="session" />
-    <Login v-else/>
+    <Login v-else />
+ 
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, toRefs } from 'vue'
 import Login from './components/Login.vue'
 import Account from './components/Account.vue'
 import Auth from './components/Auth.vue'
 import { supabase } from './supabase'
+import HomePage from './components/HomePage.vue'
 
 const session = ref()
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
     session.value = data.session
-   
+
   })
 
   supabase.auth.onAuthStateChange((_, _session) => {
