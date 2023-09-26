9c796f6f1b75a282ec4b388cad1aad4aa6789181
abdul144vista <502845@vistacollege.nl>
2023-09-26 21:45:48 +0200

    NavBar werkt nu wel

--- Mees-project/src/components/Navbar.vue
+++ Mees-project/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   
-  
+<!-- 
 <nav class="NavBar">
     <ul class="menu">
       <li><a href="#Broodjes" class="menu-btn">Broodjes</a></li>
@@ -9,35 +9,45 @@
       <li><a href="#Drankjes" class="menu-btn">Drankjes</a></li>
     </ul>
     <button type="submit" class="login-btn">Login</button>
-    
+
      <div class="hamburger" :class="{active : isActive}">
           <button @click="isActive= true">
           <span class="bar"></span>
           <span class="bar"></span>
           <span class="bar"></span>
           </button>
-          
-
      </div>
-     
+</nav> -->
+
+
+<nav class="NavBar">
+    <div class="hamburger" :class="{active : isActive}">
+        <button @click="isActive = !isActive">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+
+        </button>
+        <button v-if="isActive" class="close-btn" @click="isActive = false">
+        
+     </button>
+    </div>
+    <ul class="menu" :class="{active : isActive}">
+        <li><a href="#Broodjes" class="menu-btn">Broodjes</a></li>
+        <li><a href="#Snacks" class="menu-btn">Snacks</a></li>
+        <li><a href="#Soep" class="menu-btn">Soep</a></li>
+        <li><a href="#Drankjes" class="menu-btn">Drankjes</a></li>
+    </ul>
+    <button type="submit" class="login-btn">Login</button>
 </nav>
+
+
 
 </template>
 
 <script setup>
 import { ref } from 'vue';
-
-
-
-
-// const menu = document.querySelector('.menu');
-// const hamburger = document.querySelector('.hamburger');
 const isActive = ref(false)
-// function toggleMenu() {
-//   menu.classList.toggle('active');
-// }
-
-// hamburger.addEventListener('click', toggleMenu);
 
 
 
