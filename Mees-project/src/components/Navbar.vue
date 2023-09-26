9837aef36aaac745cb2fe1afb2e68df83d40d1e2
abdul144vista <502845@vistacollege.nl>
2023-09-27 00:39:00 +0200

    navbar

--- Mees-project/src/components/Navbar.vue
+++ Mees-project/src/components/Navbar.vue
@@ -1,25 +1,5 @@
 <template>
   
-<!-- 
-<nav class="NavBar">
-    <ul class="menu">
-      <li><a href="#Broodjes" class="menu-btn">Broodjes</a></li>
-      <li><a href="#Snacks" class="menu-btn">Snacks</a></li>
-      <li><a href="#Soep" class="menu-btn">Soep</a></li>
-      <li><a href="#Drankjes" class="menu-btn">Drankjes</a></li>
-    </ul>
-    <button type="submit" class="login-btn">Login</button>
-
-     <div class="hamburger" :class="{active : isActive}">
-          <button @click="isActive= true">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-          </button>
-     </div>
-</nav> -->
-
-
 <nav class="NavBar">
     <div class="hamburger" :class="{active : isActive}">
         <button @click="isActive = !isActive">
