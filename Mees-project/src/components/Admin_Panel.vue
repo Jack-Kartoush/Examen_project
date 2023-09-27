e1a7c8acae124e1e5917960205d3d2017e36cd46
Jack Kartoush <jackkartosh@gmail.com>
2023-09-27 13:25:42 +0200

    edit fuction af gemaakt

--- Mees-project/src/components/Admin_Panel.vue
+++ Mees-project/src/components/Admin_Panel.vue
@@ -10,26 +10,23 @@
     <input type="text" id="name" v-model="prodName" />
     <button type="submit" @click="createProduct()">add</button>
   </div>
-  <ul
-    v-for="product in Products"
-    :key="product"
-    :id="product.prod_id"
-    style="display: flex; justify-content: space-around"
-  >
-    <li>{{ product.prod_name }}</li> 
-    
-    <button v-if="!editProd" @click="editProduct(product)">
-      Bewerken
-    </button>
-    <div class="edit-form" v-else>
+  <ul>
+    <li
+      style="display: flex; justify-content: space-around; padding: 20px"
+      v-for="(product, index) in Products"
+      :key="product"
+      :id="product.prod_id"
+    >
+     <p> {{ product.prod_name }} </p>
+      <button v-if="!product.index" @click="product.index = !product.index">Bewerken</button>
+      <div class="edit-form" v-else>
+        <label for="ProdName">Product name</label>
+        <input type="text" v-model="editProdName"/>
 
-      <label for="ProdName">Product name</label>
-      <input type="text" v-model="product.prodname" />
-
-      
-      <button type="submit" @click="saveProduct(product)">save</button>
-    </div>
-    <button @click="delProduct(product)">verwijderen</button>
+        <button type="submit" @click="saveProduct(product)">save</button>
+      </div>
+      <button @click="delProduct(product)">verwijderen</button>
+    </li>
   </ul>
 
   <div>
@@ -46,12 +43,10 @@ const { session } = toRefs(props);
 
 const loading = ref(true);
 const addProd = ref(false);
-const editProd = ref(false);
 const Products = ref([]);
 const prodName = ref("");
 
 const editProdName = ref("");
-const editProdPrice = ref();
 
 
 // const username = ref('')
@@ -61,7 +56,6 @@ const editProdPrice = ref();
 onMounted(() => {
   //getProfile()
   getAllProducts();
-  
 });
 
 async function getAllProducts() {
@@ -76,28 +70,23 @@ async function getAllProducts() {
     Products.value = data;
   }
 }
-function editProduct(product ){
-  const elementID = document.getElementById(product.prod_id);
-  console.log("product id", elementID);
-  
-}
 async function saveProduct(product) {
-  let id = ref(product.prod_id);
-  console.log(id.value);
+  const id = ref(product.prod_id);
+  console.log("id ",id.value);
+  console.log("product.editProdName  ", product.prod_name );
+  console.log("editProdName  ", editProdName.value );
   const { error } = await supabase
     .from("Products")
-    .update({ prod_name: product.prodname})
+    .update({ prod_name: editProdName.value})
     .eq("prod_id", id.value);
 
   getAllProducts();
+  editProdName.value = null
 }
 async function delProduct(product) {
   let id = ref(product.prod_id);
   console.log(id.value);
-  const { error } = await supabase
-    .from("Products")
-    .delete()
-    .eq("prod_id", id.value);
+  const { error } = await supabase.from("Products").delete().eq("prod_id", id.value);
   getAllProducts();
 }
 
@@ -109,8 +98,6 @@ async function createProduct() {
   addProd.value = false;
   prodName.value = null;
 }
-
-
 
 async function signOut() {
   try {
