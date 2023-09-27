55b89ba6f90ae2a5d008afbc8f6a3072b3c0dee7
KevinScheper <kevin.scheper@hotmail.nl>
2023-09-27 09:38:01 +0200

    adding an component for the slideshow and adding an scss file + finshing the slidshow

--- Mees-project/src/components/BroodSection.vue
+++ Mees-project/src/components/BroodSection.vue
@@ -1,62 +1,35 @@
 <template>
-<div class="container">
-            <div class="slider">
-                <input type="radio" class="radio" name="images" id="radio-1" checked>
-                <input type="radio" class="radio" name="images" id="radio-2" checked>
-                <input type="radio" class="radio" name="images" id="radio-3" checked>
-                <input type="radio" class="radio" name="images" id="radio-4" checked>
-                <input type="radio" class="radio" name="images" id="radio-5" checked>
-
-                <div class="slide" id="slide-1">
-                    <img src="../assets/img/broodje2.webp" alt="">
-                    <div class="caption"><span></span></div>
-                </div>
-                <div class="slide" id="slide-2">
-                    <img src="../assets/img/broodje.jpg" alt="">
-                    <div class="caption"><span></span></div>
-                </div>
-                <div class="slide" id="slide-3">
-                    <img src="image/image3.jpg" alt="">
-                    <div class="caption"><span></span></div>
-                </div>
-                <div class="slide" id="slide-4">
-                    <img src="image/image4.jpg" alt="">
-                    <div class="caption"><span></span></div>
-                </div>
-                <div class="slide" id="slide-5">
-                    <img src="image/image5.jpg" alt="">
-                    <div class="caption"><span></span></div>
-                </div>
-                
-                <div class="dots">
-                    <label for="radio-1" id="label-1"></label>
-                    <label for="radio-2" id="label-2"></label>
-                    <label for="radio-3" id="label-3"></label>
-                    <label for="radio-4" id="label-4"></label>
-                    <label for="radio-5" id="label-5"></label>
-
-                </div>
-
-            </div>
-            
-        </div>
-
-
-<section class="Vega-Broodjes" id="VegaBroodjes" >
+    <Slideshow />
+  <section class="Vega-Broodjes" id="VegaBroodjes">
     section 1
-</section>
+    <img src="../assets/img/broodje.jpg" alt="Nature" class="responsive" />
+    <br />
+   
+  </section>
 
-<section class="Vlees-Broodjes" id="VleesBroodjes" >
+  <section class="Vlees-Broodjes" id="VleesBroodjes">
     section 2
-</section>
+  
+    <br />
+  
+  </section>
 
-<section class="Kip-Broodjes" id="KipBroodjes" >
+  <section class="Kip-Broodjes" id="KipBroodjes">
     section 3
-</section>
+   
+    <br />
+  
+  </section>
 
-<section class="Vis-Broodjes" id="VisBroodjes" >
+  <section class="Vis-Broodjes" id="VisBroodjes">
     section 4
-</section>
-
+  
+    <br />
+  
+  </section>
 </template>
+<script setup>
+import Slideshow from './Slideshow.vue';
 
+
+</script>
---
+++ Mees-project/src/components/Slideshow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="slider">
+      <input type="radio" class="radio" name="images" id="radio-1" checked />
+      <input type="radio" class="radio" name="images" id="radio-2" checked />
+      <input type="radio" class="radio" name="images" id="radio-3" checked />
+      <input type="radio" class="radio" name="images" id="radio-4" checked />
+      <input type="radio" class="radio" name="images" id="radio-5" checked />
+
+      <div class="slide" id="slide-1">
+        <img src="../assets/img/broodje2.webp" alt="" />
+        <div class="caption"><span></span></div>
+      </div>
+      <div class="slide" id="slide-2">
+        <img src="../assets/img/broodje.jpg" alt="" />
+        <div class="caption"><span></span></div>
+      </div>
+      <div class="slide" id="slide-3">
+        <img src="../assets/img/kroket.webp" alt="" />
+        <div class="caption"><span></span></div>
+      </div>
+      <div class="slide" id="slide-4">
+        <img src="../assets/img/fris.jpg" alt="" />
+        <div class="caption"><span></span></div>
+      </div>
+      <div class="slide" id="slide-5">
+        <img src="../assets/img/soep.jpg" alt="" />
+        <div class="caption"><span></span></div>
+      </div>
+
+      <div class="dots">
+        <label for="radio-1" id="label-1"></label>
+        <label for="radio-2" id="label-2"></label>
+        <label for="radio-3" id="label-3"></label>
+        <label for="radio-4" id="label-4"></label>
+        <label for="radio-5" id="label-5"></label>
+      </div>
+    </div>
+  </div>
+</template>
