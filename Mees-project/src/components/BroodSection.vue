fe01f03592bd9d7fc5277e5bbd9e28bd0594be4b
Jack Kartoush <jackkartosh@gmail.com>
2023-10-02 12:08:55 +0200

    adjusting the navstyling and broodsections

---
+++ Mees-project/src/components/BroodSection.vue
@@ -0,0 +1,62 @@
+<template>
+  <Slideshow />
+  <section class="BroodContainer" id="VegaBroodjes">
+    <!-- <div class="prod_title">
+            <span>Vega broodje</span>
+            <span>3.99</span>
+          </div> -->
+    <div class="title_container">
+      <h1 class="title">Vega Broodjes</h1>
+    </div>
+
+    <div class="prod_container">
+      <div class="prod_wraper">
+        <img src="../assets/img/broodje.jpg" alt="Nature" class="responsive" />
+        <div class="prod_title">
+          <span>Vega broodje</span>
+          <span>3.99</span>
+        </div>
+        <p class="text">
+          Als we het hebben over een goed alternatief voor een hamburger, dan
+          denken wij gelijk aan de vegetarische variant van oesterzwammen. Je
+          maakt ze in een handomdraai en ze zijn écht net zo lekker als een
+          vleesburger! In dit recept brengen we de burgers op smaak met een
+          heerlijk frisse, zelfgemaakte dragonmayonaise.
+        </p>
+      </div>
+
+      <div class="prod_wraper">
+        <img src="../assets/img/broodje.jpg" alt="Nature" class="responsive" />
+        <div class="prod_title">
+          <span>Vega broodje</span>
+          <span>3.99</span>
+        </div>
+        <p class="text">
+          Als we het hebben over een goed alternatief voor een hamburger, dan
+          denken wij gelijk aan de vegetarische variant van oesterzwammen. Je
+          maakt ze in een handomdraai en ze zijn écht net zo lekker als een
+          vleesburger! In dit recept brengen we de burgers op smaak met een
+          heerlijk frisse, zelfgemaakte dragonmayonaise.
+        </p>
+      </div>
+
+      <div class="prod_wraper">
+        <img src="../assets/img/broodje.jpg" alt="Nature" class="responsive" />
+        <div class="prod_title">
+          <span>Vega broodje</span>
+          <span>3.99</span>
+        </div>
+        <p class="text">
+          Als we het hebben over een goed alternatief voor een hamburger, dan
+          denken wij gelijk aan de vegetarische variant van oesterzwammen. Je
+          maakt ze in een handomdraai en ze zijn écht net zo lekker als een
+          vleesburger! In dit recept brengen we de burgers op smaak met een
+          heerlijk frisse, zelfgemaakte dragonmayonaise.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import Slideshow from "./Slideshow.vue";
+</script>
